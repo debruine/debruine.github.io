1abb9f8d26c8152acb1cc2f4133df2d3449970e2
Lisa DeBruine <debruine@gmail.com>
2017-11-15 20:08:42 +0000

    updated hidden solutions and added solveme

---
+++ rguppies.js
@@ -0,0 +1,63 @@
+<script>
+/* initialise hidden solutions */
+
+hidden_solution_init = function() {  
+  var buttons = document.getElementsByTagName("button");
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function() {
+      var cl = this.parentElement.classList;
+      if (cl.contains('open')) {
+        cl.remove("open");
+      } else {
+        cl.add("open");
+      }
+    }
+  }
+}
+
+/* initialise solvemes */
+  
+tc = function() {
+  document.getElementById("total_correct").innerHTML =       
+    document.getElementsByClassName("correct").length + " of " +
+    document.getElementsByClassName("solveme").length + " correct";
+}
+  
+solveme_init = function() {
+  var solveme = document.getElementsByClassName("solveme");
+  for (var i = 0; i < solveme.length; i++) {
+    solveme[i].setAttribute("autocomplete","off");
+    solveme[i].value = "";
+    solveme[i].onkeyup = function(e) {
+      var real_answer = this.getAttribute("answer").trim();
+      var my_answer = this.value;
+      var cl = this.classList;
+      if (cl.contains("nospaces")) {
+        real_answer = real_answer.replace(/ /g, "");
+        my_answer = my_answer.replace(/ /g, "");
+      }
+      if (cl.contains("ignorecase")) {
+        real_answer = real_answer.toLowerCase();
+        my_answer = my_answer.toLowerCase();
+      }
+      var linend = new RegExp(/\s*(:or:)\s*/, 'g')
+      real_answer = real_answer.split(linend);
+      if (my_answer !== "" & real_answer.includes(my_answer)) {
+        cl.add("correct");
+      } else {
+        cl.remove("correct");
+      }
+      tc();
+    }
+    solveme[i].onchange = function() {
+      this.onkeyup();
+    }
+  }
+}
+  
+window.onload = function() {
+  hidden_solution_init();
+  solveme_init();
+}
+
+</script>
